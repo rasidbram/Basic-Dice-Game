d4a6da61a81f2ab5daf96dc3d315832c2c7fda35
Your Name <rashidaynur.ibram@yandex.com>
2020-01-18 15:10:23 +0000

    works properly!

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-//for first dice
-var randomNumber1=Math.floor(Math.random()*6+1);
 
-console.log(randomNumber1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage='dice'+randomNumber1+'.png';
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource='images/'+ randomDiceImage;
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1= document.querySelectorAll('img')[0];
+var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute('src',randomImageSource);
-// for second dice
-var randomNumber2=Math.floor(Math.random()*6+1);
-console.log(randomNumber2);
+image1.setAttribute("src", randomImageSource);
 
-var randomImageSource2='images/dice'+ randomNumber2+'.png';
 
-document.querySelectorAll('img')[1].setAttribute('src',randomImageSource2);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-// conditions
-if(randomNumber1>randomNumber2){
-    document.querySelector('h1').innerHTML='Winner is player1'
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-}if(randomNumber1===randomNumber2){
-    document.querySelector('h1').innerHTML='No winner'
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-}else{
-    document.querySelector('h1').innerHTML='Winner is player2'
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
